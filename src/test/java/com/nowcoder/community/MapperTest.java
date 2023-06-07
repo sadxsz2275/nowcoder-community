29eeda159b121544ccb81f781d904a3c80ac004e
xsz <2275213309@qq.com>
2023-06-07 15:59:13 +0800

    牛客网第一章

---
+++ src/test/java/com/nowcoder/community/MapperTest.java
@@ -0,0 +1,42 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MapperTest {
+    @Autowired
+    UserMapper userMapper;
+
+    @Autowired
+    DiscussPostMapper discussPostMapper;
+
+    @Test
+    public void testSelectUser() {
+        User user = userMapper.selectById(1);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testSelectPost() {
+        List<DiscussPost> discussPosts = discussPostMapper.selectDiscussPosts(149, 0, 10);
+        for (int i = 0; i < discussPosts.size(); i++) {
+            System.out.println(discussPosts.get(i).toString());
+        }
+        int i = discussPostMapper.selectDiscussPostRows(149);
+        System.out.println(i);
+    }
+
+}
